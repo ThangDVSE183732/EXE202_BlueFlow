fa1e1a1efe10766a6b8364cf0da61127c59fa99f
Định Trần <dinht1706@gmail.com>
2025-10-20 08:54:53 +0700

    AI Chatbox

--- BE/EventLink/Eventlink_Services/Service/OpenAIService.cs
+++ BE/EventLink/Eventlink_Services/Service/OpenAIService.cs
@@ -40,7 +40,7 @@ namespace Eventlink_Services.Service
             {
                 // Get the base directory of the application
                 var basePath = AppDomain.CurrentDomain.BaseDirectory;
-                var knowledgePath = Path.Combine(basePath, "knowledge", "system_docs.txt");
+                var knowledgePath = Path.Combine(basePath, "Knowledge", "System_docs.txt");
 
                 // Check if file exists
                 if (!File.Exists(knowledgePath))
